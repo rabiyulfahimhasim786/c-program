1118fb264435d8eb2351571f97563c412278d116
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-12-29 04:28:11 -0800

    Update integerandcharacters.c

--- integerandcharacters.c
+++ integerandcharacters.c
@@ -2,9 +2,14 @@
 # include <stdio.h>
 
 int main() {
+    printf("First Program");
     int a = 7;
     printf("%d",a);
     char ch = 'r';
-    printf("%c", ch);
+    printf("\n%c", ch);
+    float pi = 3.14;
+    printf("\n%f", pi);
+    printf("\nThe value of pi is %f", pi);
+    
     return 0;
 }
